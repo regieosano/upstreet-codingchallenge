4dcc578d1584beed3144048c1793c12fa5371021
Regie Osano <rosano@paritycorp.com>
2020-11-14 21:00:09 -0800

    Added main - "try...catch" block on function clientInfoCheck

--- dist/controllers/kyc/clientInfoCheck.js
+++ dist/controllers/kyc/clientInfoCheck.js
@@ -5,56 +5,61 @@ const moment = require("moment");
 const clientInfoValidation_1 = require("../../utility/validations/clientInfoValidation");
 const axios = require("axios");
 exports.clientInfoCheck = (req, res) => {
-    const dateOfBirth = req.body.dateOfBirth;
-    const expiryDate = req.body.expiryDate;
-    const isDOBFormatValid = moment(dateOfBirth, "YYYY-MM-DD", true).isValid();
-    const isExpiryFormatValid = moment(expiryDate, "YYYY-MM-DD", true).isValid();
-    if (isDOBFormatValid && isExpiryFormatValid) {
-        const errorDetails = clientInfoValidation_1.validateClientInfo(req.body).error;
-        if (errorDetails) {
-            res.json({ error: errorDetails.details[0].message });
+    try {
+        const dateOfBirth = req.body.dateOfBirth;
+        const expiryDate = req.body.expiryDate;
+        const isDOBFormatValid = moment(dateOfBirth, "YYYY-MM-DD", true).isValid();
+        const isExpiryFormatValid = moment(expiryDate, "YYYY-MM-DD", true).isValid();
+        if (isDOBFormatValid && isExpiryFormatValid) {
+            const errorDetails = clientInfoValidation_1.validateClientInfo(req.body).error;
+            if (errorDetails) {
+                res.json({ error: errorDetails.details[0].message });
+            }
+            else {
+                const options = {
+                    headers: {
+                        "Authorization": "Bearer 03aa7ba718da920e0ea362c876505c6df32197940669c5b150711b03650a78cf",
+                        "Content-Type": "application/json"
+                    }
+                };
+                axios.post(`https://australia-southeast1-reporting-290bc.cloudfunctions.net/driverlicence
+               `, {
+                    birthDate: req.body.dateOfBirth,
+                    givenName: req.body.firstName,
+                    middleName: req.body.middleName,
+                    familyName: req.body.lastName,
+                    licenceNumber: req.body.licenceNumber,
+                    stateOfIssue: req.body.stateOfIssue,
+                    expiryDate: req.body.expiryDate
+                }, options).then((response) => {
+                    const returnCode = response.data.verificationResultCode;
+                    switch (returnCode) {
+                        case "Y":
+                            res.json({ kycResult: true });
+                            break;
+                        case "N":
+                            res.json({ kycResult: false });
+                            break;
+                        case "D":
+                            res.json({ message: "Document Error" });
+                            break;
+                        case "S":
+                            res.json({ message: "Server Error" });
+                            break;
+                    }
+                }).catch((err) => {
+                    res.json({
+                        error: err
+                    });
+                });
+            }
         }
         else {
-            const options = {
-                headers: {
-                    "Authorization": "Bearer 03aa7ba718da920e0ea362c876505c6df32197940669c5b150711b03650a78cf",
-                    "Content-Type": "application/json"
-                }
-            };
-            axios.post(`https://australia-southeast1-reporting-290bc.cloudfunctions.net/driverlicence
-            `, {
-                birthDate: req.body.dateOfBirth,
-                givenName: req.body.firstName,
-                middleName: req.body.middleName,
-                familyName: req.body.lastName,
-                licenceNumber: req.body.licenceNumber,
-                stateOfIssue: req.body.stateOfIssue,
-                expiryDate: req.body.expiryDate
-            }, options).then((response) => {
-                const returnCode = response.data.verificationResultCode;
-                switch (returnCode) {
-                    case "Y":
-                        res.json({ kycResult: true });
-                        break;
-                    case "N":
-                        res.json({ kycResult: false });
-                        break;
-                    case "D":
-                        res.json({ message: "Document Error" });
-                        break;
-                    case "S":
-                        res.json({ message: "Server Error" });
-                        break;
-                }
-            }).catch((err) => {
-                res.json({
-                    error: err
-                });
-            });
+            res.json({ error: "Invalid Date Formats - Should be YYYY-MM-DD" });
         }
     }
-    else {
-        res.json({ error: "Invalid Date Formats - Should be YYYY-MM-DD" });
+    catch (error) {
+        res.json({ error });
     }
 };
 //# sourceMappingURL=clientInfoCheck.js.map
--- src/controllers/kyc/clientInfoCheck.ts
+++ src/controllers/kyc/clientInfoCheck.ts
@@ -8,59 +8,66 @@ const axios = require("axios");
 
 export const clientInfoCheck = (req: express.Request,
                                 res: express.Response) => {
+    try {
+        // Extract dates for validation                                   
+        const dateOfBirth = req.body.dateOfBirth;
+        const expiryDate = req.body.expiryDate;
 
-    // Extract dates for validation                                   
-    const dateOfBirth = req.body.dateOfBirth;
-    const expiryDate = req.body.expiryDate;
+        const isDOBFormatValid = moment(dateOfBirth, "YYYY-MM-DD", true).isValid();
+        const isExpiryFormatValid = moment(expiryDate, "YYYY-MM-DD", true).isValid();
 
-    const isDOBFormatValid = moment(dateOfBirth, "YYYY-MM-DD", true).isValid();
-    const isExpiryFormatValid = moment(expiryDate, "YYYY-MM-DD", true).isValid();
+        if (isDOBFormatValid && isExpiryFormatValid) {
 
-    if (isDOBFormatValid && isExpiryFormatValid) {
-        const errorDetails = validateClientInfo(req.body).error                              
+           const errorDetails = validateClientInfo(req.body).error                              
 
-        if (errorDetails) {
-            res.json({error: errorDetails.details[0].message}) 
-        } else {
-            // Headers declaration
-            const options = {
-              headers: {
-               "Authorization": "Bearer 03aa7ba718da920e0ea362c876505c6df32197940669c5b150711b03650a78cf",
-               "Content-Type": "application/json"
-           }}
+           if (errorDetails) {
+               res.json({error: errorDetails.details[0].message}) 
+           } else {
+               // Headers declaration
+               const options = {
+                 headers: {
+                  "Authorization": "Bearer 03aa7ba718da920e0ea362c876505c6df32197940669c5b150711b03650a78cf",
+                  "Content-Type": "application/json"
+               }}
 
-            //  * Store key into process environment variables  
-            axios.post(`https://australia-southeast1-reporting-290bc.cloudfunctions.net/driverlicence
-            `,    
-           { 
-              birthDate: req.body.dateOfBirth,
-              givenName: req.body.firstName,
-              middleName: req.body.middleName,
-              familyName: req.body.lastName,
-              licenceNumber: req.body.licenceNumber,
-              stateOfIssue: req.body.stateOfIssue,
-              expiryDate: req.body.expiryDate 
-           }, 
-           options
+               //  * Store key into process environment variables  
+               axios.post(`https://australia-southeast1-reporting-290bc.cloudfunctions.net/driverlicence
+               `,    
+              { 
+                 birthDate: req.body.dateOfBirth,
+                 givenName: req.body.firstName,
+                 middleName: req.body.middleName,
+                 familyName: req.body.lastName,
+                 licenceNumber: req.body.licenceNumber,
+                 stateOfIssue: req.body.stateOfIssue,
+                 expiryDate: req.body.expiryDate 
+              },
+                 // Header config   
+                 options
                         
-           ).then((response: any) => {
-              // Extract verification code   
-              const returnCode = response.data.verificationResultCode;
-              switch (returnCode) {
-                 case "Y": res.json({ kycResult: true });  break;
-                 case "N": res.json({ kycResult: false }); break;
-                 case "D": res.json({ message: "Document Error" }); break;
-                 case "S": res.json({ message: "Server Error" }); break;   
-              }     
-          }).catch((err: Error) => {
-              res.json({
-                 error: err
-              })
-          })
-       }                                  
+              ).then((response: any) => {
+                 // Extract verification code   
+                 const returnCode = response.data.verificationResultCode;
+                 switch (returnCode) {
+                    case "Y": res.json({ kycResult: true });  break;
+                    case "N": res.json({ kycResult: false }); break;
+                    case "D": res.json({ message: "Document Error" }); break;
+                    case "S": res.json({ message: "Server Error" }); break;   
+                 }     
+             }).catch((err: Error) => {
+                 res.json({
+                   error: err
+                 })
+             })
+          }                                  
        
-    } else {
-        res.json({error: "Invalid Date Formats - Should be YYYY-MM-DD"})
+       } else {
+           res.json({error: "Invalid Date Formats - Should be YYYY-MM-DD"})
+       } 
+       
+    } catch (error) {
+        res.json({error}) 
     } 
+  
     
 }
